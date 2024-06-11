1e66b83d3775ef128ee550a80bca9ae86fe5c4e1
aidavyy <aiidasophea@gmail.com>
2024-06-11 18:57:27 +0800

    fix error frontend faq on app.js

--- main-frontend/src/App.js
+++ main-frontend/src/App.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 import FAQ from "./faq/faq.js"
 import Helpdesk from "./Helpdesk/Helpdesk.js"
 import Feedback from "./feedback/feedback.jsx"
-import Analysis from "./analysisPage/Analysis";
+import Analysis from "./analysisPage/Analysis"
 import React from 'react'
 import Landing from './landing/Landing.jsx'
 import Register from './uauth/Register.jsx'
@@ -12,16 +11,10 @@ import OTPVerif from './uauth/OTPVerif.jsx'
 import Dashboard from './dashboard/Dashboard.jsx'
 // import Layout from './shared/Layout.jsx'
 import {Routes, Route} from 'react-router-dom'
-=======
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SubscriptionPage from './SubscriptionFolder/SubscriptionPage';
->>>>>>> 2c0aaf2a4d3b733172effe371d8a01e2fb5e2860
 
 
 function App() {
   return (
-<<<<<<< HEAD
     <Routes>
       <Route path='/' element = {<Landing/>}/>
       <Route path='/register' element = {<Register/>}/>
@@ -29,29 +22,19 @@ function App() {
       <Route path='/forgetpass' element = {<ForgetPass/>}/>
       <Route path='/otpverif' element = {<OTPVerif/>}/>
       <Route 
-        path='/dashboard' 
+        path='/dashboard.html' 
         element = {
           // <Layout>
           // </Layout>
         <Dashboard/>
         }
       />
-      <Route path='/analysis' element={<Analysis/>}/>
+      <Route path='/analysis.html' element={<Analysis/>}/>
       <Route path='/faq' element = {<FAQ/>}/>
-      <Route path='/helpdesk' element = {<Helpdesk/>}/>
-      <Route path='/feedback' element = {<Feedback/>}/>
+      <Route path='/helpdesk.html' element = {<Helpdesk/>}/>
+      <Route path='/feedback.html' element = {<Feedback/>}/>
     </Routes>
   );
-=======
-    <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SubscriptionPage />} />
-      </Routes>
-    </BrowserRouter>
-    </div>
-  )
->>>>>>> 2c0aaf2a4d3b733172effe371d8a01e2fb5e2860
 }
 
 export default App;
