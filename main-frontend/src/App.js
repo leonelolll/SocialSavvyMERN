c76c3a6ed1b6f5e36a8b25b1e2e09f1e4c3cbbc7
adamarbain <u2101735@siswa.um.edu.my>
2024-06-12 11:02:09 +0800

    chore: Update routes and links for FAQ, Feedback, and Help Desk pages

--- main-frontend/src/App.js
+++ main-frontend/src/App.js
@@ -32,9 +32,9 @@ function App() {
       <Route path='/calendar' element={<Calendar />} />
       <Route path='/ContentAnalysis' element={<ContentAnalysis />} />
       <Route path='/analysis' element={<Analysis />} />
-      <Route path='/dashboard/faq' element={<FAQ />} />
-      <Route path='/dashboard/helpdesk' element={<Helpdesk />} />
-      <Route path='/dashboard/feedback' element={<Feedback />} />
+      <Route path='/faq' element={<FAQ />} />
+      <Route path='/helpdesk' element={<Helpdesk />} />
+      <Route path='/feedback' element={<Feedback />} />
     </Routes>
   );
 }
--- main-frontend/src/shared/Navbar.jsx
+++ main-frontend/src/shared/Navbar.jsx
@@ -26,9 +26,9 @@ function Navbar() {
             <div className={s.dropdown}>
                 <button className={s.dropbtn}>Help  &#11206;</button>
                 <div className={s.dropdown_content}>
-                    <Link to="faq" className={s.a}>FAQ</Link>
-                    <Link to="feedback" className={s.a}>Feedback</Link>
-                    <Link to="helpdesk" className={s.a}>Help Desk</Link>
+                    <Link to="/faq" className={s.a}>FAQ</Link>
+                    <Link to="/feedback" className={s.a}>Feedback</Link>
+                    <Link to="/helpdesk" className={s.a}>Help Desk</Link>
                 </div>
             </div>
             <div className={s.right}>
