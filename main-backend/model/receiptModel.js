f6d2ac30f7719cb01c082be17a2809a420759626
yuurisha <117178128+yuurisha@users.noreply.github.com>
2024-06-12 12:42:18 +0800

    Update receiptModel.js

--- main-backend/model/receiptModel.js
+++ main-backend/model/receiptModel.js
@@ -1,46 +1,45 @@
 const receipts = [
-    {
-      date: '23 May 2024',
-      receiptNumber: '#00004',
-      paymentMethod: 'Card (#### #### #### ####)',
-      retailer: 'SocialSavvy',
-      address: 'Bangsar, Kuala Lumpur',
-      vatId: '19000003',
-      product: 'Premium Plan',
-      amountPaid: 'MYR 99.99',
-    },
-    {
-      date: '23 April 2024',
-      receiptNumber: '#00003',
-      paymentMethod: 'Card (#### #### #### ####)',
-      retailer: 'SocialSavvy',
-      address: 'Bangsar, Kuala Lumpur',
-      vatId: '19000003',
-      product: 'Premium Plan',
-      amountPaid: 'MYR 99.99',
-    },
-    {
-      date: '23 March 2024',
-      receiptNumber: '#00002',
-      paymentMethod: 'Card (#### #### #### ####)',
-      retailer: 'SocialSavvy',
-      address: 'Bangsar, Kuala Lumpur',
-      vatId: '19000003',
-      product: 'Premium Plan',
-      amountPaid: 'MYR 99.99',
-    },
-    {
-      date: '23 February 2024',
-      receiptNumber: '#00001',
-      paymentMethod: 'Card (#### #### #### ####)',
-      retailer: 'SocialSavvy',
-      address: 'Bangsar, Kuala Lumpur',
-      vatId: '19000003',
-      product: 'Basic Plan',
-      amountPaid: 'MYR 59.99',
-    }
-    // Add more static receipt objects as needed
-  ];
+  {
+    date: '23 May 2024',
+    receiptNumber: '#00004',
+    paymentMethod: 'Card (#### #### #### ####)',
+    retailer: 'SocialSavvy',
+    address: 'Bangsar, Kuala Lumpur',
+    vatId: '19000003',
+    product: 'Premium Plan',
+    amountPaid: 'MYR 99.99',
+  },
+  {
+    date: '23 April 2024',
+    receiptNumber: '#00003',
+    paymentMethod: 'Card (#### #### #### ####)',
+    retailer: 'SocialSavvy',
+    address: 'Bangsar, Kuala Lumpur',
+    vatId: '19000003',
+    product: 'Premium Plan',
+    amountPaid: 'MYR 99.99',
+  },
+  {
+    date: '23 March 2024',
+    receiptNumber: '#00002',
+    paymentMethod: 'Card (#### #### #### ####)',
+    retailer: 'SocialSavvy',
+    address: 'Bangsar, Kuala Lumpur',
+    vatId: '19000003',
+    product: 'Premium Plan',
+    amountPaid: 'MYR 99.99',
+  },
+  {
+    date: '23 February 2024',
+    receiptNumber: '#00001',
+    paymentMethod: 'Card (#### #### #### ####)',
+    retailer: 'SocialSavvy',
+    address: 'Bangsar, Kuala Lumpur',
+    vatId: '19000003',
+    product: 'Basic Plan',
+    amountPaid: 'MYR 59.99',
+  }
   
-  
-  module.exports = receipts;
+];
+
+export default receipts;
